d6afe539b8d7169c1105f41873befd27f2f72a5f
monarul007 <monarul007@gmail.com>
2022-03-17 12:09:06 +0600

    fixed redirection for admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,7 +32,7 @@ class AppServiceProvider extends ServiceProvider
         }
         $appType = env('CLIENT_TYPE');
         $setting = MetaSettings::firstWhere('client', env('CLIENT_ID'));
-        $design = Design::firstWhere(['client' => env('CLIENT_ID'), 'form' => $setting->form ?? '']);
+        $design = Design::firstWhere(['client' => env('CLIENT_ID'), 'form' => $setting->form ?? 'form-1']);
         View::share(['app_type' => $appType,'design' => $design, 'setting' => $setting]);
     }
 }
